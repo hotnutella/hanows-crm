4402d5484d000c91e597667a2a7cdaf759623949
Deniss Suhhanov <desuhh@taltech.ee>
2024-04-05 14:51:20 +0300

    move styles outside the app folder

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,19 @@
 'use client';
 
 import { useGetClientsQuery } from "../../store/clientsApi";
-import styles from "./page.module.css";
-import { Button } from "@mui/material";
+import { Button, Container } from "@mui/material";
 
 export default function Home() {
   const { data: clients } = useGetClientsQuery({});
 
   return (
-    <div className={styles.container}>
+    <Container>
       <Button variant="contained" color="primary">
         Hello World
       </Button>
       <div>
         {JSON.stringify(clients, null, 2)}
       </div>
-    </div>
+    </Container>
   );
 }
