773f634bc4bc783bf6a204957b289e4b96bb8e1c
Deniss Suhhanov <desuhh@taltech.ee>
2024-05-05 14:43:20 +0300

    Remove unused code

--- src/app/@chat/[id]/[invoice_id]/page.tsx
+++ src/app/@chat/[id]/[invoice_id]/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useEffect } from 'react';
+import React from 'react';
 import { useGetInvoiceQuery } from '@/store/api/invoicesApi';
 import ChatHeader from '@/components/chat/ChatHeader';
 
