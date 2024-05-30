95d375299043694a0ae7d04c6173f9e71d7b3963
Deniss Suhhanov <desuhh@taltech.ee>
2024-05-30 19:39:18 +0300

    Add no clients found message

--- src/app/crm/@clients/page.tsx
+++ src/app/crm/@clients/page.tsx
@@ -2,7 +2,7 @@
 
 import React from 'react';
 import { useGetClientsQuery } from '../../../store/api/clientsApi';
-import { Stack } from '@mui/material';
+import { Stack, Typography } from '@mui/material';
 import ClientOption from '@/components/clients/ClientOption';
 import ClientsHeader from '@/components/clients/ClientsHeader';
 import { useSelector } from 'react-redux';
@@ -22,10 +22,15 @@ const ClientsPage = () => {
         <Stack spacing={2} mx={1}>
             <ClientsHeader />
             {filteredClients.map((client) => (
-                <ClientOption 
-                    key={client.id} 
+                <ClientOption
+                    key={client.id}
                     client={client} />
             ))}
+            {filteredClients.length === 0 && (
+                <Stack justifyContent="center" alignItems="center">
+                    <Typography variant="body2">No clients found</Typography>
+                </Stack>
+            )}
         </Stack>
     );
 };
