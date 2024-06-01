a2580dafe474b0be89f6f6b7a72489cc8024e027
Deniss Suhhanov <desuhh@taltech.ee>
2024-06-01 21:28:51 +0300

    Add action buttons to the invoices

--- src/components/chat/InvoiceMessage.tsx
+++ src/components/chat/InvoiceMessage.tsx
@@ -1,5 +1,7 @@
 import { Invoice } from '@/store/api/invoicesApi';
-import { Box, Paper, Stack, Typography } from '@mui/material';
+import { Box, IconButton, Paper, Stack, Tooltip, Typography } from '@mui/material';
+import EditIcon from '@mui/icons-material/Edit';
+import CheckIcon from '@mui/icons-material/Check';
 import React from 'react';
 import styles from './InvoiceMessage.module.css';
 import { useGetInvoiceLinesByInvoiceQuery } from '@/store/api/invoiceLinesApi';
@@ -30,25 +32,41 @@ const InvoiceMessage: React.FC<InvoiceMessageProps> = ({ invoice }) => {
     }
 
     return (
-        <Paper className={className} onClick={handleClick}>
-            <Stack direction="column" justifyContent="space-between" height="100%">
-                <Typography variant="body2">Invoice {invoice.invoice_number}</Typography>
-                <Box>
-                    <Typography variant="body2" textAlign="right" fontSize={10}>Invoice date: {invoice.issue_date}</Typography>
-                    <Typography variant="body2" textAlign="right" fontSize={10}>Due date: {invoice.due_date}</Typography>
-                </Box>
-                <Box>
-                    {invoiceLines?.map((line) => (
-                        <Stack key={line.id} direction="row" justifyContent="space-between">
-                            <Typography variant="body1" fontSize={10}>{line.description}</Typography>
-                            <Typography variant="body1" fontSize={10}>{line.quantity} x {(line.unit_price + line.unit_price * line.vat / 100).toFixed(2)}</Typography>
-                        </Stack>
-                    ))}
-                </Box>
-                <Typography variant="body1" textAlign="right">Total: {invoice.total_amount}</Typography>
-            </Stack>
-            {invoice.status === 'draft' && <Ribbon text={invoice.status} />}
-        </Paper>
+        <Stack direction="row">
+            {invoice.status === 'draft' && (
+                <Stack direction="column" justifyContent="center" className={styles.actions}>
+                    <Tooltip title="Edit" placement="top">
+                        <IconButton size="large">
+                            <EditIcon />
+                        </IconButton>
+                    </Tooltip>
+                    <Tooltip title="Confirm">
+                        <IconButton size="large">
+                            <CheckIcon />
+                        </IconButton>
+                    </Tooltip>
+                </Stack>
+            )}
+            <Paper className={className} onClick={handleClick}>
+                <Stack direction="column" justifyContent="space-between" height="100%">
+                    <Typography variant="body2">Invoice {invoice.invoice_number}</Typography>
+                    <Box>
+                        <Typography variant="body2" textAlign="right" fontSize={10}>Invoice date: {invoice.issue_date}</Typography>
+                        <Typography variant="body2" textAlign="right" fontSize={10}>Due date: {invoice.due_date}</Typography>
+                    </Box>
+                    <Box>
+                        {invoiceLines?.map((line) => (
+                            <Stack key={line.id} direction="row" justifyContent="space-between">
+                                <Typography variant="body1" fontSize={10}>{line.description}</Typography>
+                                <Typography variant="body1" fontSize={10}>{line.quantity} x {(line.unit_price + line.unit_price * line.vat / 100).toFixed(2)}</Typography>
+                            </Stack>
+                        ))}
+                    </Box>
+                    <Typography variant="body1" textAlign="right">Total: {invoice.total_amount}</Typography>
+                </Stack>
+                {invoice.status === 'draft' && <Ribbon text={invoice.status} />}
+            </Paper>
+        </Stack>
     );
 };
 
